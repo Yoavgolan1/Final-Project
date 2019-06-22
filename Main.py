d214c2b4f1ac8a0c580a30d386e7fa7a48d20010
Yoavgolan1 <yoavgolan1@gmail.com>
2019-06-22 13:09:59 +0300

    Added comments

--- Main.py
+++ Main.py
@@ -11,9 +11,9 @@ inputs_length = 2*np.round(split_ratio * trajectory_length).astype(int)
 outputs_length = 2*trajectory_length - inputs_length
 inputs = np.zeros((n_trajectories, inputs_length))
 outputs = np.zeros((n_trajectories, outputs_length))
-print(outputs.shape)
+#print(outputs.shape)
 for ii in range(n_trajectories):
-    new_traj = trj.create_trajectory(n_timesteps=trajectory_length, g=0.0981, initial_speed_interval=(0, 1))
+    new_traj = trj.create_trajectory(n_timesteps=trajectory_length, g=0.0981, initial_speed_interval=(0, 1), noise=0.02)
     new_input, new_output = trj.split_trajectory(new_traj, split_ratio)
     inputs[ii, :] = new_input
     outputs[ii, :] = new_output
--- trajectories.py
+++ trajectories.py
@@ -7,6 +7,12 @@ class Trajectories:
 
 def create_trajectory(n_timesteps=200, duration=10, initial_speed_interval=(0, 10),
                       initial_angle_interval=(15, 165), noise=2, g=9.81):
+    # create_trajectories receives a bunch of trajectory parameters, and returns a trajectory as a tuple of
+    # x and y coordinates. n_timesteps is the number of points in the trajectory, duration is the total time of
+    # flight, i.e. the trajectory starts at time 0 and ends at time 'duration'. initial_speed_interval is the interval
+    # within which the initial speed of the trajectory is selected. initial_angle_interval is the interval within
+    # which the initial trajectory angle is selected. noise is the magnitude of randomized noise in the trajectory. g
+    # is the gravitational constant. The trajectory originates from 0,0.
 
     t = np.linspace(0, duration, n_timesteps)
     initial_angle_interval = np.deg2rad(initial_angle_interval)
@@ -29,6 +35,14 @@ def create_trajectory(n_timesteps=200, duration=10, initial_speed_interval=(0, 1
 
 
 def split_trajectory(traj, split_ratio=0.8):
+    # split_trajectory recieves a trajectory, such as produced by create_trajectory, with a split ratio, and returns
+    # two vectors that represent two parts of the trajectory. The split ration decides the length of the respective
+    # returned results. i.e., if the trajectory length is 100 timesteps, and the split ratio is 0.8, the first returned
+    # value "input" will be a trajectory that is 80 time steps long, and the second returned value "output" will be
+    # a trajectory 20 timesteps long. The "input" and "output" trajectories are not tuples of x,y coordinates, but
+    # rather the x,y coordinates are appended to a single vector.
+    # I.e., "input" = [x0, x1, x2, ..., x80, y0, y1, ..., y80], "output" = [x81, x82, ..., x100, y81, y82, ..., y100]
+
     traj_x = traj[0]
     traj_y = traj[1]
     n_timesteps = len(traj_x)
@@ -50,6 +64,6 @@ def split_trajectory(traj, split_ratio=0.8):
     return input, output
 
 
-t_x = create_trajectory(20)
+#t_x = create_trajectory(20)
 
-mm, nn = split_trajectory(t_x)
+#mm, nn = split_trajectory(t_x)
