44abc50774fb7116a3d32290effbac7b432ee336
Shmulik <shmulik.ed1@gmail.com>
2019-06-24 10:19:13 +0300

    submit 1

--- trajectories.py
+++ trajectories.py
@@ -1,12 +1,12 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 class Trajectories:
     def __init__(self):
         return
 
-
 def create_trajectory(n_timesteps=200, duration=10, initial_speed_interval=(0, 10),
-                      initial_angle_interval=(15, 165), noise=2, g=9.81):
+                      initial_angle_interval=(15, 165), noise=.2, g=9.81):
     # create_trajectories receives a bunch of trajectory parameters, and returns a trajectory as a tuple of
     # x and y coordinates. n_timesteps is the number of points in the trajectory, duration is the total time of
     # flight, i.e. the trajectory starts at time 0 and ends at time 'duration'. initial_speed_interval is the interval
@@ -25,12 +25,14 @@ def create_trajectory(n_timesteps=200, duration=10, initial_speed_interval=(0, 1
     v0x = initial_speed * np.cos(initial_direction)
     v0y = initial_speed * np.sin(initial_direction)
 
-    trajectory_x = np.zeros(n_timesteps)
-    trajectory_y = np.zeros(n_timesteps)
+    # trajectory_x = np.zeros(n_timesteps)
+    # trajectory_y = np.zeros(n_timesteps)
 
-    for ii in range(n_timesteps):
-        trajectory_x[ii] = x0 + v0x * t[ii] + np.random.uniform(-noise, noise)
-        trajectory_y[ii] = y0 + v0y * t[ii] + 0.5 * (-g) * t[ii]**2 + np.random.uniform(-noise, noise)
+    trajectory_x = x0 + v0x * t + np.random.normal(size = n_timesteps) * noise
+    trajectory_y = y0 + v0y * t + 0.5 * (-g) * t**2 + np.random.normal(size = n_timesteps) * noise
+    # for ii in range(n_timesteps):
+    #     trajectory_x[ii] = x0 + v0x * t[ii] + np.random.uniform(-noise, noise)
+    #     trajectory_y[ii] = y0 + v0y * t[ii] + 0.5 * (-g) * t[ii]**2 + np.random.uniform(-noise, noise)
     return (trajectory_x, trajectory_y)
 
 
@@ -39,7 +41,7 @@ def split_trajectory(traj, split_ratio=0.8):
     # two vectors that represent two parts of the trajectory. The split ration decides the length of the respective
     # returned results. i.e., if the trajectory length is 100 timesteps, and the split ratio is 0.8, the first returned
     # value "input" will be a trajectory that is 80 time steps long, and the second returned value "output" will be
-    # a trajectory 20 timesteps long. The "input" and "output" trajectories are not tuples of x,y coordinates, but
+    # a trajectory 20 time steps long. The "input" and "output" trajectories are not tuples of x,y coordinates, but
     # rather the x,y coordinates are appended to a single vector.
     # I.e., "input" = [x0, x1, x2, ..., x80, y0, y1, ..., y80], "output" = [x81, x82, ..., x100, y81, y82, ..., y100]
 
@@ -63,6 +65,18 @@ def split_trajectory(traj, split_ratio=0.8):
 
     return input, output
 
+def plot(traj_x, traj_y):
+
+    plt.plot(traj_x, traj_y, 'o-r')
+    plt.grid()
+    plt.xlabel('X')
+    plt.ylabel('Y')
+    plt.title('Trajectory')
+    # plt.show()
+
+def inBasket(alpha = 45, vel = 5):
+
+    Basket_CPos = [10, 5]
 
 #t_x = create_trajectory(20)
 
