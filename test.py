82daf6fd2ff23e643ff0ed63280ab68dbeba95bc
Shmulik <shmulik.ed1@gmail.com>
2019-06-26 19:18:17 +0300

    submit 1

--- test.py
+++ test.py
@@ -1,19 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+x = np.random.normal(0,1,1000)*15
 
-x = np.linspace(0, 4*np.pi, 100)
-y = np.sin(x)
-
-Ttraj = True
-print() if Ttraj == True else marker = '--g'
-
-# line_style = '--'
-# line_color = 'm'
-# plt.figure()
-# plt.plot(x, y, linestyle = line_style, color = line_color, alpha=0.5)
-#
-# basket = np.asarray([[10,0], [15,0]])
-# plt.plot(basket[:,0], basket[1,:], 'rd-')
-#
-# plt.show()
+print(np.std(x))
+plt.figure()
+plt.hist(x)
+plt.show()
