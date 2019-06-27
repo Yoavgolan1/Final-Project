ce6e051fb9e2ab31355d6aa3348963e5bf17c6bb
Shmulik <shmulik.ed1@gmail.com>
2019-06-27 12:01:56 +0300

    submit 1

---
+++ Main3.py
@@ -0,0 +1,18 @@
+import numpy as np
+import trajectories2 as trj
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
+# Create training set
+n_trajectories = 100
+dt = 0.1
+time_interval = (0,90)
+initial_speed_interval = (650, 750)
+initial_angleAZ_interval = (30, 60)
+initial_angleAL_interval = (45, 50)
+trj.create_dataSet(n_trajectories = n_trajectories, dt = dt, time_interval=time_interval,
+                   initial_speed_interval=initial_speed_interval, initial_angleAZ_interval=initial_angleAZ_interval,
+                   initial_angleAL_interval=initial_angleAL_interval,
+                   noise = 20, g=9.81, plot = False)
+
